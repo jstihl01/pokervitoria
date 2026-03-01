3d76ca39a5823a5e07b4c284bc7024c202fe6bd8
Jaime Stihl <91011915+jstihl01@users.noreply.github.com>
2026-03-01 18:32:16 +0100

    Delete apps directory
    Haciendo limpieza

--- apps/server/src/routes/debug.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/debug/error", (_req, _res) => {
-  throw new Error("Debug error (intentional)");
-});
-
-module.exports = router;
